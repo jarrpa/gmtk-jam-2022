32d4bdfb00e7190dc2bcc18fcee14c41fa940768
CakeCrusher <1sebastian1sosa1@gmail.com>
2022-07-16 12:12:05 -0400

    fixed GameManager

--- Assets/Scripts/SceneManager/GameManager.cs
+++ Assets/Scripts/SceneManager/GameManager.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+    public static GameObject Manager;
     // Start is called before the first frame update
     void Awake() {
         print("GameManager Awake");
-        UnityEngine.SceneManagement.SceneManager.LoadScene("SebastianScene2");
-        // if user presses the escape key change scene to SebastianScene2
+        Manager = GameObject.Find("GameManager");
+        if (Instance != null && Instance != this) {
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(Manager);
     }
-    // void Update() {
-    //     if (Input.GetKeyDown(KeyCode.Escape)) {
-    //         print("GameManager Update");
-    //     }
-    // }
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("SebastianScene2");
+        }
+    }
 }
