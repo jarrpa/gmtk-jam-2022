1a8d1e54de3d38ae22f3ba53f963c1767e0392ee
Jose A. Rivera <jarrpa@gmail.com>
2022-12-18 19:18:48 -0600

    project: introduce SuperUnityBuild package
    Signed-off-by: Jose A. Rivera <jarrpa@gmail.com>

---
+++ Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,38 @@
+// This file is auto-generated. Do not modify or move this file.
+
+public static class BuildConstants
+{
+    public enum ReleaseType
+    {
+        None,
+        win,
+        webgl,
+    }
+
+    public enum Platform
+    {
+        None,
+        PC,
+        WebGL,
+    }
+
+    public enum Architecture
+    {
+        None,
+        WebGL,
+    }
+
+    public enum Distribution
+    {
+        None,
+        published,
+    }
+
+    public static readonly System.DateTime buildDate = new System.DateTime(638065345152457557);
+    public const string version = "2022-12-13";
+    public const ReleaseType releaseType = ReleaseType.webgl;
+    public const Platform platform = Platform.WebGL;
+    public const Architecture architecture = Architecture.WebGL;
+    public const Distribution distribution = Distribution.published;
+}
+
